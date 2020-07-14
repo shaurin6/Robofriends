214ff38e29046ddb58380e4adfb90770ed0e125b
Shaurin <shaurin2000@gmail.com>
2020-07-14 11:40:05 +0530

    Updates

---
+++ precache-manifest.c0dee6547de2612a7581a65e06e21b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe42549245da496130d5",
+    "url": "/Robofriends/static/css/main.63e02c04.chunk.css"
+  },
+  {
+    "revision": "fe42549245da496130d5",
+    "url": "/Robofriends/static/js/main.26a2b22d.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "8a71276b7ed9f6fd1cd7",
+    "url": "/Robofriends/static/css/2.f6c46c04.chunk.css"
+  },
+  {
+    "revision": "8a71276b7ed9f6fd1cd7",
+    "url": "/Robofriends/static/js/2.b98a55d3.chunk.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Robofriends/static/media/SEGA.82e09f78.woff"
+  },
+  {
+    "revision": "e300e9254789cadd143b1ec12749b278",
+    "url": "/Robofriends/index.html"
+  }
+];
